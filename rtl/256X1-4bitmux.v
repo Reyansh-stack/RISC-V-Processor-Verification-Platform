bac56cbf60743d44fb4e20cdd06f5256543a29e4
Reyansh-stack <reyanshrav3@gmail.com>
2026-07-20 19:56:51 +0530

    256X1 4 Bit Multiplexer

---
+++ rtl/256X1-4bitmux.v
@@ -0,0 +1,12 @@
+module top_module(
+    input [1023:0] in,
+    input [7:0] sel,
+    output [3:0] out
+);
+
+    assign out = in[sel*4 +: 4];
+    //in[startindex+: width]
+    //in[startindex + width -1 : startindex]
+    
+
+endmodule
